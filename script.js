10ed0019644ebd03e10652193eabae4758f93cb4
GitHubのユーザー名 <GitHub>
2025-08-08 02:21:13 +0900

    GitHub Pages用にbaseタグ追加

---
+++ script.js
@@ -0,0 +1,17 @@
+// フェードインのアニメーション処理
+const fadeElems = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+fadeElems.forEach(elem => {
+  observer.observe(elem);
+});
